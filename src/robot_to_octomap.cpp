83ef1b262f4c1084d55fb058d4a2b6a37dedc5ca
Jim Mainprice <mainprice@gmail.com>
2017-10-16 14:06:57 +0200

    Switch to std pointers model

--- src/robot_to_octomap.cpp
+++ src/robot_to_octomap.cpp
@@ -1,46 +1,43 @@
-#include <human_bio_octomap/robot_to_octomap.hpp>
-#include <moveit/robot_model/robot_model.h>
-#include <moveit/distance_field/find_internal_points.h>
-#include <geometric_shapes/body_operations.h>
-#include <urdf/model.h>
-#include <octomap/octomap.h>
-#include <gtest/gtest.h>
-#include <glog/logging.h>
 #include <Eigen/Geometry>
+#include <geometric_shapes/body_operations.h>
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+#include <human_bio_octomap/robot_to_octomap.hpp>
+#include <moveit/distance_field/find_internal_points.h>
+#include <moveit/robot_model/robot_model.h>
+#include <octomap/octomap.h>
+#include <urdf/model.h>
 
 using std::cerr;
 using std::cout;
 using std::endl;
 using namespace human_bio;
 
-std::string shape_to_string(shapes::ShapeType v)
-{
+std::string shape_to_string(shapes::ShapeType v) {
   switch (v) {
-    case shapes::ShapeType::UNKNOWN_SHAPE:
-      return "UNKNOWN_SHAPE";
-    case shapes::ShapeType::SPHERE:
-      return "SPHERE";
-    case shapes::ShapeType::CYLINDER:
-      return "CYLINDER";
-    case shapes::ShapeType::CONE:
-      return "CONE";
-    case shapes::ShapeType::BOX:
-      return "BOX";
-    case shapes::ShapeType::PLANE:
-      return "PLANE";
-    case shapes::ShapeType::MESH:
-      return "MESH";
-    case shapes::ShapeType::OCTREE:
-      return "OCTREE";
-    default:
-      return "[Unknown OS_type]";
+  case shapes::ShapeType::UNKNOWN_SHAPE:
+    return "UNKNOWN_SHAPE";
+  case shapes::ShapeType::SPHERE:
+    return "SPHERE";
+  case shapes::ShapeType::CYLINDER:
+    return "CYLINDER";
+  case shapes::ShapeType::CONE:
+    return "CONE";
+  case shapes::ShapeType::BOX:
+    return "BOX";
+  case shapes::ShapeType::PLANE:
+    return "PLANE";
+  case shapes::ShapeType::MESH:
+    return "MESH";
+  case shapes::ShapeType::OCTREE:
+    return "OCTREE";
+  default:
+    return "[Unknown OS_type]";
   }
 }
 
-void findInternalPointsConvex_local(const bodies::Body& body,
-                                    double resolution,
-                                    EigenSTL::vector_Vector3d& points)
-{
+void findInternalPointsConvex_local(const bodies::Body &body, double resolution,
+                                    EigenSTL::vector_Vector3d &points) {
   bodies::BoundingSphere sphere;
   body.computeBoundingSphere(sphere);
   double xval_s = std::floor((sphere.center.x() - sphere.radius - resolution) /
@@ -64,7 +61,7 @@ void findInternalPointsConvex_local(const bodies::Body& body,
   //  cout << " bs : radius : " << sphere.radius << endl;
   //  cout << " bs : resolution : " << resolution << endl;
 
-  double anti_alias = 0.2;  // TODO remove this eventually
+  double anti_alias = 0.2; // TODO remove this eventually
   double step = resolution * anti_alias;
   Eigen::Vector3d pt(Eigen::Vector3d::Zero());
   for (pt.x() = xval_s; pt.x() <= xval_e; pt.x() += step) {
@@ -78,8 +75,7 @@ void findInternalPointsConvex_local(const bodies::Body& body,
   }
 }
 
-Model human_bio::LoadMoveitModelFromString(const std::string& xml_robot)
-{
+Model human_bio::LoadMoveitModelFromString(const std::string &xml_robot) {
   // Get model
   cout << "Create URDF model" << endl;
   urdf::Model model;
@@ -90,14 +86,12 @@ Model human_bio::LoadMoveitModelFromString(const std::string& xml_robot)
   cout << "Create SRDF model" << endl;
   srdf::Model semantic_model;
   semantic_model.initString(*urdf_model, "");
-  boost::shared_ptr<const srdf::Model> srf_model(
-      new srdf::Model(semantic_model));
+  auto srf_model = std::make_shared<const srdf::Model>(semantic_model);
 
   return Model(new moveit::core::RobotModel(urdf_model, srf_model));
 }
 
-Model human_bio::LoadMoveitModelFromParams(const std::string& ros_param)
-{
+Model human_bio::LoadMoveitModelFromParams(const std::string &ros_param) {
   ros::NodeHandle node_handle;
 
   std::string full_ros_param;
@@ -117,23 +111,23 @@ Model human_bio::LoadMoveitModelFromParams(const std::string& ros_param)
   return human_bio::LoadMoveitModelFromString(xml_robot);
 }
 
-std::vector<bodies::Body*> ModelToBodies::GetBodies()
-{
-  bool tranform_from_base((base_frame_.matrix() -
-                           Eigen::Affine3d::Identity().matrix()).norm() > 1e-9);
+std::vector<bodies::Body *> ModelToBodies::GetBodies() {
+  bool tranform_from_base(
+      (base_frame_.matrix() - Eigen::Affine3d::Identity().matrix()).norm() >
+      1e-9);
   uint32_t k = 0;
-  std::vector<bodies::Body*> bodies;
+  std::vector<bodies::Body *> bodies;
   for (int i = 0; i < robot_model_->getLinkModels().size(); i++) {
 
-    const moveit::core::LinkModel* link = robot_model_->getLinkModels()[i];
+    const moveit::core::LinkModel *link = robot_model_->getLinkModels()[i];
     // std::string link_name = link->getName();
-    const Eigen::Affine3d& link_pose = state_->getGlobalLinkTransform(link);
+    const Eigen::Affine3d &link_pose = state_->getGlobalLinkTransform(link);
 
     for (int j = 0; j < link->getShapes().size(); j++) {
 
-      bodies::Body* body = NULL;
+      bodies::Body *body = NULL;
 
-      const Eigen::Affine3d& shape_pose =
+      const Eigen::Affine3d &shape_pose =
           link->getCollisionOriginTransforms()[j];
 
       if (bodies_.empty()) {
@@ -157,9 +151,8 @@ std::vector<bodies::Body*> ModelToBodies::GetBodies()
   return bodies_;
 }
 
-ModelsToOctomap::ModelsToOctomap(double resolution, const AABB& bb)
-    : resolution_(resolution)
-{
+ModelsToOctomap::ModelsToOctomap(double resolution, const AABB &bb)
+    : resolution_(resolution) {
   tree_ = std::shared_ptr<octomap::OcTree>(new octomap::OcTree(resolution_));
   octomap::point3d bb_min(bb.x_min, bb.y_min, bb.z_min);
   octomap::point3d bb_max(bb.x_max, bb.y_max, bb.z_max);
@@ -170,8 +163,7 @@ ModelsToOctomap::ModelsToOctomap(double resolution, const AABB& bb)
   cout << "                    dim max : " << bb_max << endl;
 }
 
-bool ModelsToOctomap::Update()
-{
+bool ModelsToOctomap::Update() {
   // Remove all points from the OcTree
   tree_->clear();
 
@@ -182,7 +174,7 @@ bool ModelsToOctomap::Update()
   uint32_t nb_points = 0;
   uint32_t nb_in_bb = 0;
   for (auto m : models_) {
-    std::vector<bodies::Body*> bodies = m->GetBodies();
+    std::vector<bodies::Body *> bodies = m->GetBodies();
 
     for (auto b : bodies) {
       EigenSTL::vector_Vector3d point_vec;
